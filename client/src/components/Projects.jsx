7fba097eb469d674978ccf0885dd58f95086bb68
Dennis Kim <dekim2324@gmail.com>
2019-12-05 15:58:12 -0600

    modal working. next is working on modal itself

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
-
+import React, { Component, useState } from 'react';
+import { Modal, Button, ButtonToolbar } from 'react-bootstrap';
 
 function Projects() {
+    
+    const [lgShow, setLgShow] = useState(false);
+    const [lgShow1, setLgShow1] = useState(false);
+    const [lgShow2, setLgShow2] = useState(false);
+    const [lgShow3, setLgShow3] = useState(false);
+    const [lgShow4, setLgShow4] = useState(false);
+    const [lgShow5, setLgShow5] = useState(false);
+
     return(
         <div style={style}>
 
             <div style={container1}>
                 <div className="card" style={{width: "25rem"}}>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/Screen%20Shot%202019-12-04%20at%207.45.44%20PM.png?alt=media&token=523fe748-e415-435d-a251-2bc342025ada" className="card-img-top" alt="..." />
+                    <img 
+                    onClick={() => setLgShow(true)}
+                    src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/Screen%20Shot%202019-12-04%20at%207.45.44%20PM.png?alt=media&token=523fe748-e415-435d-a251-2bc342025ada" 
+                    className="card-img-top" 
+                    alt="..." 
+                    
+                    />
                     <div className="card-body">
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div className="card" style={{width: "25rem"}}>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/FEC(2).png?alt=media&token=5f544cee-9092-4046-8a51-ad9baefc4504" className="card-img-top" alt="..." />
+                    <img 
+                    onClick={() => setLgShow1(true)}
+                    src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/FEC(2).png?alt=media&token=5f544cee-9092-4046-8a51-ad9baefc4504" 
+                    className="card-img-top" 
+                    alt="..." />
                     <div className="card-body">
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div className="card" style={{width: "25rem"}}>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/SDC(2).png?alt=media&token=877b0a83-88a8-40f5-ad59-b6b99bda20a7" className="card-img-top" alt="..." />
+                    <img 
+                    onClick={() => setLgShow2(true)}
+                    src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/SDC(2).png?alt=media&token=877b0a83-88a8-40f5-ad59-b6b99bda20a7" 
+                    className="card-img-top" 
+                    alt="..." />
                     <div className="card-body">
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
@@ -29,30 +51,126 @@ function Projects() {
 
             <div style={container2}>
                 <div className="card" style={{width: "25rem"}}>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/TrainingTracker(2).png?alt=media&token=e5844c77-4d24-4f95-a6cd-370d816183e8" className="card-img-top" alt="..." />
+                    <img 
+                    onClick={() => setLgShow3(true)}
+                    src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/TrainingTracker(2).png?alt=media&token=e5844c77-4d24-4f95-a6cd-370d816183e8" 
+                    className="card-img-top" 
+                    alt="..." />
                     <div className="card-body">
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div className="card" style={{width: "25rem"}}>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/twiddler(1).png?alt=media&token=ed6ea4c3-d49b-41cc-9288-82f237c2910b" className="card-img-top" alt="..." />
+                    <img 
+                    onClick={() => setLgShow4(true)}
+                    src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/twiddler(1).png?alt=media&token=ed6ea4c3-d49b-41cc-9288-82f237c2910b" 
+                    className="card-img-top" 
+                    alt="..." />
                     <div className="card-body">
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div className="card" style={{width: "25rem"}}>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/current(1).png?alt=media&token=d5aa7fa8-1413-48d9-8c0b-4cb538a79678" className="card-img-top" alt="..." />
+                    <img 
+                    onClick={() => setLgShow5(true)}
+                    src="https://firebasestorage.googleapis.com/v0/b/mvp-360.appspot.com/o/current(1).png?alt=media&token=d5aa7fa8-1413-48d9-8c0b-4cb538a79678" 
+                    className="card-img-top" 
+                    alt="..." />
                     <div className="card-body">
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
             </div>
-  
 
+
+            <Modal
+                size="lg"
+                show={lgShow}
+                onHide={() => setLgShow(false)}
+                aria-labelledby="example-modal-sizes-title-lg"
+                >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-lg">
+                    iCook365
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>...</Modal.Body>
+            </Modal>
+
+            <Modal
+                size="lg"
+                show={lgShow1}
+                onHide={() => setLgShow1(false)}
+                aria-labelledby="example-modal-sizes-title-lg"
+                >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-lg">
+                    ShareBnB
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>...</Modal.Body>
+            </Modal>
+
+            <Modal
+                size="lg"
+                show={lgShow2}
+                onHide={() => setLgShow2(false)}
+                aria-labelledby="example-modal-sizes-title-lg"
+                >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-lg">
+                    Pi-Kea
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>...</Modal.Body>
+            </Modal>
+
+            <Modal
+                size="lg"
+                show={lgShow3}
+                onHide={() => setLgShow3(false)}
+                aria-labelledby="example-modal-sizes-title-lg"
+                >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-lg">
+                    Training Tracker
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>...</Modal.Body>
+            </Modal>
+
+            <Modal
+                size="lg"
+                show={lgShow4}
+                onHide={() => setLgShow4(false)}
+                aria-labelledby="example-modal-sizes-title-lg"
+                >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-lg">
+                    Twiddler
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>...</Modal.Body>
+            </Modal>
+
+            <Modal
+                size="lg"
+                show={lgShow5}
+                onHide={() => setLgShow5(false)}
+                aria-labelledby="example-modal-sizes-title-lg"
+                >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-lg">
+                    Dennis Personal Web
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>...</Modal.Body>
+            </Modal>
 
         </div>
     )
 }
+
 
 const style = {
     paddingTop: '20px',
